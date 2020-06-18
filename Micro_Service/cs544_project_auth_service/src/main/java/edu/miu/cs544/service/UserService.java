63d515abe3e0d6862e9a4726e7220fa5120ae1eb
Nina <thilenguyen.dao@gmail.com>
2020-06-17 22:00:57 -0500

    get all users

--- Micro_Service/cs544_project_auth_service/src/main/java/edu/miu/cs544/service/UserService.java
+++ Micro_Service/cs544_project_auth_service/src/main/java/edu/miu/cs544/service/UserService.java
@@ -1,7 +1,10 @@
 package edu.miu.cs544.service;
 
+import java.util.List;
+
 import edu.miu.cs544.domain.User;
 
 public interface UserService {
 	User getByUsername(String username);
+	List<User> getAll();
 }
--- Micro_Service/cs544_project_auth_service/src/main/java/edu/miu/cs544/service/UserServiceImpl.java
+++ Micro_Service/cs544_project_auth_service/src/main/java/edu/miu/cs544/service/UserServiceImpl.java
@@ -1,5 +1,7 @@
 package edu.miu.cs544.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +17,11 @@ public class UserServiceImpl implements UserService {
 	@Override
 	public User getByUsername(String username) {
 		return userRepository.findByUsername(username).get();
+	}
+
+	@Override
+	public List<User> getAll() {
+		return userRepository.findAll();
 	}
 
 }
